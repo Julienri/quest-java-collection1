76cfc2c887adc90d3c5f8059ecffbb18cdbe40a6
Julien Risacher <julien.risacher@gmail.com>
2019-06-18 17:51:52 +0200

    thanos

--- Thanos.java
+++ Thanos.java
@@ -1,12 +1,42 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Thanos {
 
     public static void main(String[] args) {
 
         // TODO 1 : Create an empty heroes list
-
+        ArrayList<Hero> heroes = new ArrayList<>();
         // TODO 2 : Add those heroes to the list
+        Hero black = new Hero("Black Widow", 34);
+        Hero captain = new Hero("Captain America", 100);
+        Hero vision = new Hero("Vision", 3);
+        Hero iron = new Hero("Iron Man", 48);
+        Hero scarlet = new Hero("Scarlet Witch", 29);
+        Hero thor = new Hero ("Thor", 1500);
+        Hero spider = new Hero("Spider-Man", 18);
+        Hero hulk = new Hero("Hulk", 49);
+        Hero doctor = new Hero ("Doctor Strange", 42);
+
+        heroes.add(black);
+        heroes.add(captain);
+        heroes.add(vision);
+        heroes.add(iron);
+        heroes.add(scarlet);
+        heroes.add(thor);
+        heroes.add(spider);
+        heroes.add(hulk);
+        heroes.add(doctor);
+
+        thor.setAge(1501);
+
+        Collections.shuffle(heroes);
+        heroes.subList(0,heroes.size()/2).clear();
+
+        for(int i=0; i <heroes.size(); i++){
+            System.out.println(heroes.get(i).getName());
+        }
+    } 
         // name: Black Widow, age: 34
         // name: Captain America, age: 100
         // name: Vision, age: 3
@@ -25,4 +55,3 @@ public class Thanos {
 
         // TODO 6 : Loop throught the list and display the name of the remaining heroes
     }
-}
